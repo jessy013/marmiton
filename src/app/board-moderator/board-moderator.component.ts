2503a5f24980053cbdef0d4a3daba1eee8ea15b9
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-09-23 15:51:46 +0200

    up

--- src/app/board-moderator/board-moderator.component.ts
+++ src/app/board-moderator/board-moderator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-board-moderator',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board-moderator.component.scss']
 })
 export class BoardModeratorComponent implements OnInit {
+  [x: string]: any;
+  content?: string;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getModeratorBoard().subscribe(
+      (data: string | undefined) => {
+        this.content = data;
+      },
+      (err: { error: string; }) => {
+        this.content = JSON.parse(err.error).message;
+      }
+    );
   }
-
 }
--- src/app/board-user/board-user.component.ts
+++ src/app/board-user/board-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-board-user',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board-user.component.scss']
 })
 export class BoardUserComponent implements OnInit {
+  content?: string;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getUserBoard().subscribe(
+      (data: string | undefined) => {
+        this.content = data;
+      },
+      (err: { error: string; }) => {
+        this.content = JSON.parse(err.error).message;
+      }
+    );
   }
-
 }
